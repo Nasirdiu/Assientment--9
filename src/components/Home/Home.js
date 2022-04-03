5e83c577ca4f9b183c9f3afdec3aa143019da177
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-03 23:00:54 +0600

    home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import Bike from "../../Asstes/Bike.jpg";
+import './Home.css'
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="header-left">
+        <h1 className="text-success">My Next Bike Suzuki Gixxer</h1>
+        <p>The 2022 Gixxer promises even more dynamic riding experience with advanced looks and features setting up its own standard.</p>
+        <button className="btn btn-info w-25">Suzuki Gixxer</button>
+      </div>
+      <div className="">
+        <img className="img-fluid w-100" src={Bike} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
